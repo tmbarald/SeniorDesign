50cd25ab5d152b60b872e93d9ec337a1bb21e1d9
tmbarald <35852234+tmbarald@users.noreply.github.com>
2019-12-01 18:41:06 -0500

    Add files via upload

--- lines.py
+++ lines.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as mp
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('hotgif.gif')
 #cap = cv2.VideoCapture('green.mp4')
 green_over_time = []
@@ -22,10 +22,11 @@ while(cap.isOpened()):
     #convert frame of video cap to HSV spectrum
     test = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #range of green pixels (play with)
-    mask = cv2.inRange(test, (25, 25, 25), (80, 255, 255))
+    mask = cv2.inRange(test, (40, 0, 240), (120, 100, 255))
     croped = cv2.bitwise_and(frame, frame, mask=mask)
     gray = cv2.cvtColor(croped, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("gray", gray)
+    cv2.imshow("test", test)
+    cv2.imshow("croped", croped)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
     kernel = np.ones((4,4), np.uint8)
     dilation = cv2.dilate(edges, kernel, iterations=1)
