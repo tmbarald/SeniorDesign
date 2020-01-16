862a720d383419e56281b7aef2ccb6112bbe87cc
tmbarald <35852234+tmbarald@users.noreply.github.com>
2020-01-16 17:09:33 -0500

    updated them gud

--- GUI_Code/GUI.py
+++ GUI_Code/GUI.py
@@ -1,38 +1,47 @@
-from tkinter import *
+import tkinter as tk
 import os
 import sys
 import cv2
 from PIL import ImageTk, Image
+import time
+import numpy as np
+from capture import VideoCapture
 
-cap = cv2.VideoCapture(1)
+class UserInterface:
+    def __init__(self, window, title):
+        self.window = window
+        self.window.title(title)
+        self.vid = VideoCapture()
+        self.canvas = tk.Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+        self.capture_button = tk.Button(window, text="Begin Capture", command=self.begin_capture)
+        self.capture_button.pack()
+        self.stop_button = tk.Button(window, text="Stop Capture", state="disabled", command=self.stop_capture)
+        self.stop_button.pack()
+        self.delay = 1
+        self.update()
 
-def start():
-    _, frame = cap.read()
-   # cv2.imshow("frame", frame)
-    image_feed = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    #cv2.imshow("image_feed", image_feed)
-    img = Image.fromarray(image_feed)
-  #  cv2.imshow("img", img)
-    imgtk = ImageTk.PhotoImage(image=img)
-    video_part.imgtk = imgtk
-    video_part.configure(image=imgtk)
-    video_part.after(1, start)
-    print("hello boi")
+        self.window.mainloop()
 
-#print("hello world")
-master = Tk()
-master.title("Minecraft")
-master.geometry('700x400')
-#frame = Frame(master)
-#frame.pack()
-canvas = Frame(master, bg="white")
-#canvas.pack()
-canvas.grid()
-video_part = Label(canvas)
-video_part.grid()
-redbutton = Button(canvas, text = 'Capture', fg ='red', command=start)
-redbutton.grid()
-master.mainloop()
+    def begin_capture(self):
+        self.capture_button["state"] = "disabled"
+        self.stop_button["state"] = "normal"
+        print("Started here")
 
+    def stop_capture(self):
+        self.capture_button["state"] = "normal"
+        self.stop_button["state"] = "disabled"
+        print("Stopped here")
 
 
+#Sends video frames to the gui, no slowdown so far
+    def update(self):
+        ret, frame = self.vid.get_frame()
+        if ret:
+            self.imgtk = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.imgtk, anchor=tk.NW)
+        self.canvas.after(1, self.update)
+
+
+
+master = UserInterface(tk.Tk(), "Minecraft")
--- GUI_Code/capture.py
+++ GUI_Code/capture.py
@@ -1,28 +1,35 @@
+import tkinter as tk
+import os
+import sys
 import cv2
+from PIL import ImageTk, Image
+import time
 import numpy as np
 
-#cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('hotgif.gif')
-#cap = cv2.VideoCapture('green.mp4')
-#green_over_time = []
-#frames_over_time = []
-#time = []
+#video class
+class VideoCapture:
+    def __init__(self):
+        self.vid = cv2.VideoCapture(1)
+        if not self.vid.isOpened():
+            raise ValueError("source is wrong", 0)
 
-#frames = 0
-#fps = cap.get(cv2.CAP_PROP_FPS)
-#print(fps)
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.vid.set(cv2.CAP_PROP_FOURCC, 0x32595559)
+        self.vid.set(cv2.CAP_PROP_FPS, 15)
 
-def run_cap():
-    print("hello boooi")
-    cap = cv2.VideoCapture(1)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    while(cap.isOpened()):
-       ret, frame = cap.read()
-       cv2.imshow("frame", frame)
-       if((cv2.waitKey(1) & 0xFF == ord('q')) or  (cv2.getWindowProperty('frame', 0) == -1)):
-           break
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
 
-    cap.release()
-    cv2.destroyAllWindows()
-
-
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
